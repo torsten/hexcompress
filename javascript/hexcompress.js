dfc55c606fa7b24984096ebaf338a4a299f8ed70
Torsten Becker <torsten.becker@gmail.com>
2014-11-28 14:37:29 -0500

    Javascript decompression.

---
+++ javascript/hexcompress.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function decompress(buffer) {
+  var result = new Buffer(buffer.length * 2);
+  var r = 0;
+
+  for (var i=0; i < buffer.length; i++) {
+    var byte = buffer[i];
+    // 33: !, 126: ~
+    if (byte != 0 && (byte < 33 || byte > 126)) {
+      var hex = byte.toString(16)
+      if (byte < 16) {
+        hex = "0" + hex;
+      }
+      result[r] = hex.charCodeAt(0);
+      result[r + 1] = hex.charCodeAt(1);
+      r += 2;
+    }
+    else {
+      result[r] = byte;
+      r++;
+    }
+  };
+
+  return result.toString("ascii", 0, r);
+}
+
+module.exports = {
+  decompress: decompress
+};
---
+++ javascript/hexcompress_spec.js
@@ -0,0 +1,43 @@
+var sinon = require('sinon');
+require('should');
+
+var hexcompress = require('../lib/hexcompress');
+
+describe('hexcompress.decompress', function() {
+
+  it('keeps ascii', function() {
+    hexcompress.decompress(new Buffer("asd")).should.equal("asd");
+  });
+
+  it('decodes hex', function() {
+    hexcompress.decompress(new Buffer([0x61, 0x73, 0xdf])).should.equal("asdf");
+  });
+
+  it('decodes capital ascii', function() {
+    hexcompress.decompress(new Buffer("asdF")).should.equal("asdF");
+  });
+
+  it('decodes single hex', function() {
+    hexcompress.decompress(new Buffer([0x1f])).should.equal("1f");
+  });
+
+  it('decodes single hex with follow-up', function() {
+    hexcompress.decompress(new Buffer([0x1f, 0x41])).should.equal("1fA");
+  });
+
+  it('decodes single hex with prefix', function() {
+    hexcompress.decompress(new Buffer([0x7a, 0x1f])).should.equal("z1f");
+  });
+  
+  it('keeps 00', function() {
+    hexcompress.decompress(new Buffer("00")).should.equal("00");
+  });
+
+  it('keeps null byte', function() {
+    hexcompress.decompress(new Buffer([0x00])).should.equal("\u0000");
+  });
+
+  it('keeps 41', function() {
+    hexcompress.decompress(new Buffer("41")).should.equal("41");
+  });
+});
